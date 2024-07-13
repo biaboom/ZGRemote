0a4449f42f1f581fbad46cecfd6a43789c31c072
imhzg <imhzg@outlook.com>
2024-07-13 16:08:05 +0800

    update DelegateHandlerBase

--- src/ZGRemote.Common/Processor/DelegateHandlerBase.cs
+++ src/ZGRemote.Common/Processor/DelegateHandlerBase.cs
@@ -9,7 +9,7 @@ namespace ZGRemote.Common.Processor
 {
     
     public delegate void Excute(UserContext user, IMessage message);
-    public abstract class DelegateHandlerBase<T> : IDisposable where T : DelegateHandlerBase<T>, new()
+    public abstract class DelegateHandlerBase<T> where T : DelegateHandlerBase<T>, new()
     {
         protected static ConcurrentDictionary<UserContext, T> userInstanceTable = new ConcurrentDictionary<UserContext, T>();
         protected bool disposedValue;
@@ -45,7 +45,7 @@ namespace ZGRemote.Common.Processor
         {
             if(userInstanceTable.TryRemove(user, out T instance))
             {
-                instance.Dispose();
+                instance.UserContext = null;
             }
         }
 
@@ -63,28 +63,6 @@ namespace ZGRemote.Common.Processor
             throw new Exception("the Excute mothod must override");
         }
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: 释放托管状态(托管对象)
-                    UserContext = null;
-                }
-
-                // TODO: 释放未托管的资源(未托管的对象)并重写终结器
-                // TODO: 将大型字段设置为 null
-                disposedValue = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
     }
 
     //标记Handle能处理哪些Message
